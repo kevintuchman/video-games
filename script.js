06a2b7207b932e828f5f18e19b20fb4db544b69a
kevintuchman <kevin.a.tuchman@gmail.com>
2019-06-22 17:53:16 -0700

    j

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-console.log("Hi, You")
+document.write('<h1>Hello</h1>');
 
-var username;
-console.log(username);
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
 
-username = "You"
-console.log(username)
+if (hourNow > 18) {
+    greeting = 'Good evening from Seattle!';
+} else if (hourNow > 12)  {
+    greeting = 'Good afternoon from Seattle!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning from Seattle!';
+} else {
+  greeting = 'Welcome';
+}
+
+document.write('<h3>' + greeting + '</h3>');
+
